accf66d654287de9fc06a6968627e2e30c75de9a
wanglei.77 <wanglei.77@bytedance.com>
2018-02-08 22:10:01 +0800

    feat: use middleware
    Change-Id: Idae5e288e6faa8aaa3b62d8f773f36eb4640f5fd

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx, next) => {
+  const sTime = new Date().getTime()
   await next()
+  const eTime = new Date().getTime()
   ctx.response.type = 'text/html'
   ctx.response.body = '<h1>Hello world!</h1>'
+  console.log(`请求地址：${ctx.path}，响应时间：${eTime - sTime}ms`)
+})
+
+app.use(async (ctx, next) => {
+  console.log('中间件1 do something')
+  await next()
+  console.log('中间件1 end')
+})
+
+app.use(async (ctx, next) => {
+  console.log('中间件2 do something')
+  // await next()
+  console.log('中间件2 end')
+})
+
+app.use(async (ctx, next) => {
+  console.log('中间件3 do something')
+  await next()
+  console.log('中间件3 end')
 })
 
 app.listen(3000, () => {
