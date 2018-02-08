d16ccad78bbb47cd52f63037c9a52d4fe6c1764f
wanglei.77 <wanglei.77@bytedance.com>
2018-02-08 22:41:47 +0800

    chore: 使用koa-router管理路由
    Change-Id: Ic55fd1874e9e49f2338f696c95ac4cb9d359f757

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
 const Koa = require('koa')
 const app = new Koa()
+const router = require('koa-router')()
 
-app.use(async (ctx, next) => {
-  const sTime = new Date().getTime()
-  await next()
-  const eTime = new Date().getTime()
-  ctx.response.type = 'text/html'
-  ctx.response.body = '<h1>Hello world!</h1>'
-  console.log(`请求地址：${ctx.path}，响应时间：${eTime - sTime}ms`)
+// 初识中间件middleware
+// app.use(async (ctx, next) => {
+//   const sTime = new Date().getTime()
+//   await next()
+//   const eTime = new Date().getTime()
+//   ctx.response.type = 'text/html'
+//   ctx.response.body = '<h1>Hello world!</h1>'
+//   console.log(`请求地址：${ctx.path}，响应时间：${eTime - sTime}ms`)
+// })
+//
+// app.use(async (ctx, next) => {
+//   console.log('中间件1 do something')
+//   await next()
+//   console.log('中间件1 end')
+// })
+//
+// app.use(async (ctx, next) => {
+//   console.log('中间件2 do something')
+//   // await next()
+//   console.log('中间件2 end')
+// })
+//
+// app.use(async (ctx, next) => {
+//   console.log('中间件3 do something')
+//   await next()
+//   console.log('中间件3 end')
+// })
+
+// 如果不使用koa-router或者其它路由中间件，自己手动实现路由，可以如下：
+// app.use(async (ctx, next) => {
+//   if (ctx.request.url === '/') {
+//     ctx.response.body = '<h1>index page</h1>'
+//   } else {
+//     await next()
+//   }
+// })
+//
+// app.use(async (ctx, next) => {
+//   if (ctx.request.url === '/home') {
+//     ctx.response.body = '<h1>home page</h1>'
+//   } else {
+//     await next()
+//   }
+// })
+//
+// app.use(async (ctx, next) => {
+//   if (ctx.request.url === '/404') {
+//     ctx.response.body = '<h1>404 page</h1>'
+//   } else {
+//     await next()
+//   }
+// })
+
+// 添加路由
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>index page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件1 do something')
-  await next()
-  console.log('中间件1 end')
+router.get('/home', async (ctx, next) => {
+  ctx.response.body = '<h1>home page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件2 do something')
-  // await next()
-  console.log('中间件2 end')
+router.get('/404', async (ctx, next) => {
+  ctx.response.body = '<h1>404 page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件3 do something')
-  await next()
-  console.log('中间件3 end')
-})
+app.use(router.routes())
 
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
